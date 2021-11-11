7cbc4f9d9bba3fd90a41ea86c05da18f1a3c1d85
Anastasiia F <aviktorovna2510@gmail.com>
2021-11-11 15:11:13 +0200

    index.js changed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const recordRouter = require("./routes/record.routes")
-const PORT = process.env.PORT || 8000;
+const PORT_BE = process.env.PORT || 8000;
+const PORT_UI = 8001;
 const app = express();
 app.use(express.json())
 app.use("/api", recordRouter)
+app.use(express.static(require('path').resolve(__dirname, '..', "frontend")));
 
-
-app.listen(PORT, () => console.log(`server started on port: ${PORT} `))
+app.listen(PORT_UI, () => console.log(`frontend started on port:  ${PORT_UI}`));
+app.listen(PORT_BE, () => console.log(`server started on port: ${PORT_BE} `))
